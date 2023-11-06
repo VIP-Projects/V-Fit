3fba7dc9c03e09e4c4b2e1428c42774191f7457e
JunYong Kim <62223905+wragon@users.noreply.github.com>
2023-11-06 11:32:57 +0900

    Update u2net_run.py

--- Virtual_Fitting/U2Net/u2net_run.py
+++ Virtual_Fitting/U2Net/u2net_run.py
@@ -1,3 +1,35 @@
+'''
+프로젝트 V-Fit 변경 사항 문서
+=============================
+이 문서는 프로젝트 V-Fit의 변경 사항을 문서화하고, Apache 2.0 라이선스에 따라 라이선스 정보와 고지 사항을 제공합니다.
+
+변경 사항
+-----------
+1. 변경 사항 목적 및 이유:
+   - 모듈 경로를 변경하여 코드 구성을 향상시키고 가독성을 개선하였습니다.
+
+2. 변경된 파일 및 경로:
+   - 변경 전: `from u2net_test import normPRED`
+   - 변경 후: `from U2Net.u2net_test import normPRED`
+
+   - 변경 전: `from data_loader import RescaleT`
+   - 변경 후: `from U2Net.data_loader import RescaleT`
+
+   - 변경 전: `from data_loader import ToTensor`
+   - 변경 후: `from U2Net.data_loader import ToTensor`
+
+   - 변경 전: `from data_loader import ToTensorLab`
+   - 변경 후: `from U2Net.data_loader import ToTensorLab`
+
+   - 변경 전: `from data_loader import SalObjDataset`
+   - 변경 후: `from U2Net.data_loader import SalObjDataset`
+   
+라이선스 정보
+--------------
+프로젝트 V-Fit는 Apache 2.0 라이선스 하에 배포됩니다. Apache 2.0 라이선스의 전체 텍스트는 "LICENSE" 파일에서 확인할 수 있습니다.
+변경 사항은 Apache 2.0 라이선스에 따라 배포됩니다.
+'''
+
 import os
 from skimage import io, transform
 import torch
